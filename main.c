26101fd5c3b812ac54b688a011fa86be0973b651
Ahmad Ismail <a.h.ismail.dev@gmail.com>
2023-02-08 18:58:06 +0200

    Add test mode to verify calculation validity.

--- main.c
+++ main.c
@@ -13,16 +13,62 @@ int main(int argc, char **argv)
     // Calculate expressions sent as command line arguments.
     if (argc > 1)
     {
-        for (int i = 1; i < argc; ++i)
+        if (strcmp(argv[1], "--test") == 0)
         {
-            ans = calculate_expr(argv[i], false);
-            if (isnan(creal(ans)))
+            // Load the test file, should have the following format:
+            // expression1;expected_answer1
+            // expression2;expected_answer2
+            // ...
+            FILE *test_file;
+            test_file = fopen(argv[2], "r");
+            if (test_file == NULL)
+
             {
-                error_handler(NULL, 2);
-                continue;
+                fputs("Unable to open test file.\n", stderr);
+                exit(1);
             }
-            printf("%.14g\n", creal(ans));
+            char buffer[500];
+            int separator;
+            while (feof(test_file) == 0)
+            {
+                fgets(buffer, 500, test_file);
+                separator = f_search(buffer, ";", 0,false);
+                if (separator == -1)
+                {
+                    fputs("Incorrect format for test file, missing ;", stderr);
+                    continue;
+                }
+
+                char expr[separator];
+                double expected_ans;
+                sscanf(buffer+separator+1, "%lf", &expected_ans);
+                strncpy(expr,buffer,separator);
+                expr[separator]='\0';
+                puts(expr);
+                scientific_complex_picker(expr);
+                // Calculate relative error
+                double relative_error = fabs((expected_ans - ans) / expected_ans);
+                printf("Expected=%g, result=%g, relative error=%g",expected_ans,ans,relative_error);
+                if (relative_error > 1e-3)
+                {
+                    fputs("Relative error is too high.", stderr);
+                    exit(1);
+                }
+                else
+                    puts("Passed");
+            }
         }
+        else
+            for (int i = 1; i < argc; ++i)
+            {
+                ans = calculate_expr(argv[i], false);
+                if (isnan(creal(ans)))
+                {
+                    error_handler(NULL, 2);
+                    continue;
+                }
+                printf("%.14g\n", creal(ans));
+            }
         return 0;
     }
 
